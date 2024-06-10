651635f83a359a0c72bb4b38e6fe01c3c04e1e97
AbdullahOztoprak <e210503006@stud.tau.edu.tr>
2024-06-10 17:40:46 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <ESPAsyncWebServer.h>
 #include "../config.h" 
 
-
 // Pins for ultrasonic sensor
 #define TRIGGER_PIN 5
 #define ECHO_PIN 18
@@ -21,6 +20,8 @@ int getDistance() {
   // Implement the logic to measure the distance using the ultrasonic sensor
   // and return the distance in centimeters
   // For example:
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
   digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, LOW);
@@ -30,7 +31,6 @@ int getDistance() {
 
 // Function to set up the web server routes
 void setupServer() {
-  // Serve the HTML page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     String html = R"(
       <html>
@@ -43,9 +43,9 @@ void setupServer() {
               if (xhr.status == 200) {
                 var data = JSON.parse(xhr.responseText);
                 document.getElementById('distance').innerText = data.distance + ' cm';
-                document.getElementById('led1').innerText = data.led1;
-                document.getElementById('led2').innerText = data.led2;
-                document.getElementById('led3').innerText = data.led3;
+                document.getElementById('led1').innerText = data.led1 ? 'ON' : 'OFF';
+                document.getElementById('led2').innerText = data.led2 ? 'ON' : 'OFF';
+                document.getElementById('led3').innerText = data.led3 ? 'ON' : 'OFF';
               }
             };
             xhr.send();
@@ -65,7 +65,6 @@ void setupServer() {
     request->send(200, "text/html", html);
   });
 
-  // Provide data in JSON format
   server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
     int distance = getDistance();
     String json = "{";
@@ -81,20 +80,16 @@ void setupServer() {
 }
 
 void setup() {
-  // Initialize Serial Monitor
   Serial.begin(115200);
   
-  // Initialize digital pins as outputs
   pinMode(LED1_PIN, OUTPUT);
   pinMode(LED2_PIN, OUTPUT);
   pinMode(LED3_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
 
-  // Initialize ultrasonic sensor pins
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 
-  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -102,14 +97,13 @@ void setup() {
   }
   Serial.println("Connected to WiFi");
 
-  // Set up the web server
   setupServer();
 }
 
 void loop() {
   int distance = getDistance();
 
-  if(0 < distance &&  distance < 5){
+  if (0 < distance && distance < 5) {
     digitalWrite(LED1_PIN, HIGH);
     digitalWrite(LED2_PIN, HIGH);
     digitalWrite(LED3_PIN, HIGH);
@@ -123,7 +117,7 @@ void loop() {
     digitalWrite(LED1_PIN, HIGH);
     digitalWrite(LED2_PIN, HIGH);
     digitalWrite(LED3_PIN, HIGH);
-    tone(BUZZER_PIN, 1000); // Play tone at 1000 Hz
+    tone(BUZZER_PIN, 1000);
     delay(150);
     noTone(BUZZER_PIN);
     delay(150);
@@ -133,7 +127,7 @@ void loop() {
     digitalWrite(LED3_PIN, LOW);
     tone(BUZZER_PIN, 1000);
     delay(300);
-    noTone(BUZZER_PIN); // Stop the tone
+    noTone(BUZZER_PIN);
     delay(300);
   } else if (distance < 30) {
     digitalWrite(LED1_PIN, HIGH);
@@ -141,13 +135,14 @@ void loop() {
     digitalWrite(LED3_PIN, LOW);
     tone(BUZZER_PIN, 1000);
     delay(500);
-    noTone(BUZZER_PIN); // Stop the tone
+    noTone(BUZZER_PIN);
     delay(500);
   } else {
     digitalWrite(LED1_PIN, LOW);
     digitalWrite(LED2_PIN, LOW);
     digitalWrite(LED3_PIN, LOW);
-    noTone(BUZZER_PIN); // Stop the tone
+    noTone(BUZZER_PIN);
     delay(1000);
   }
 }
+
